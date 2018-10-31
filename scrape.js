5e1c284e06d393c96cc27a27a9210d025a7b6fe0
CaffeineKing <jacy_yahoo.com>
2018-10-30 18:27:46 -0700

    remove scrape.js

--- scrape.js
+++
@@ -1,9 +0,0 @@
-var axios = require("axios");
-var cheerio = require("cheerio");
-var request = require('request-promise');
-
-axios.get("http://redtri.com/mom-hacks/national/").then(function(response) {
-  console.log(response.data);
-  var $ = cheerio.load(response.data)
-})
-
