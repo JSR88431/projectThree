e4101caf0f2a7d0b8afde4d0e0545c7a877c9ed6
CaffeineKing <jacy_yahoo.com>
2018-11-02 06:34:57 -0700

    attempt to add cards

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -2,45 +2,57 @@
 import Calendar from "./Calendar.js"
 import React from 'react';
 import { Container, Row, Col } from 'reactstrap';
+import axios from "axios";
+import Calendarcard from "./calendarcomp/calendarCard.js"
 
 class Search extends React.Component {
     state = {
         results: [],
         clickedDay: ""
     }
-
-    displayCalendarSearch = (e) =>{
-      
-            console.log(e, "hurray you made it this far");
-       
+    //load cards immediately
+    componentDidMount() {
+      // after component loads, get all products from db
+      axios.get("/api/allMomsLaClasses").then(response => {
+        this.setState({
+          results: response.data
+        });
+      });
     }
 
-   
-
-
+    displayCalendarSearch = () =>{
+      console.log((this.state.clickedDay))
+      // console.log((this.state.results[0]))
+    }
   render() {
-    //   console.log((this.state.clickedDay))
+
     return (
       <Container>
         <Row>
-          <Col>.col</Col>
-                </Row>
-                     <Row>
-               <Col xs="6" sm="4"></Col>
-                <Col xs="6" sm="4"></Col>
-             <Col sm="4">
-            <Calendar 
-                 onClickDay={e =>this.displayCalendarSearch(e)}
-            />
-          
-                
-          </Col>
-        </Row>       
-      </Container>
-
-
+          <Col>
+            <Calendarcard 
+                src = {this.state.results.image}
+                title = {this.state.results.title}
+                href = {this.state.results.link}
+                src = {this.state.results.image}
+               />
+            </Col>
+         </Row>
+           <Row>
+             <Col xs="6" sm="4"></Col>
+              <Col xs="6" sm="4"></Col>
+                <Col sm="4">
+                   <Calendar 
+                      onClickDay={(day) => 
+                        this.setState({
+                          clickedDay: day
+                         })}
+                         displayCalendarSearch={this.displayCalendarSearch()}
+                      />
+                </Col>
+             </Row>       
+       </Container>
     );
   }
 }
-
 export default Search;
