5882eab886a1358a2ba0b54f4eb45c32b7b2e12f
ajz003 <ajz003@ucla.edu>
2018-11-05 13:53:19 -0800

    move section creator into cards

--- client/src/components/forumlevels/Topic.js
+++ client/src/components/forumlevels/Topic.js
@@ -16,7 +16,28 @@ const captionStyle ={
 }
 
 const Topic = (props) => {
-    
+
+
+    // let section = props.topicResults.map(item => {
+    //     // create a route-able link for each item
+    //     return (
+    //         <li className="list-group-item" key={item.id}>
+    //             <a
+    //                 href="#"
+    //                 onClick={e => props.handleLevelChange(e, "Post")}
+    //                 id={item.id}
+    //                 txt={item.title}
+    //             >
+    //                 <h1>{item.title}</h1>
+    //             </a>
+    //             <p>Original Poster: {item.owner}</p>
+    //             <p>Number of Posts in Thread: {item.postCount}</p>
+    //             <a href="#" id={item.id} onClick={props.deleteATopic} owner={item.owner}>Delete</a>
+    //         </li>
+    //     );
+    // });
+
+
     return (
         <div className="forum-element">
             <section>
@@ -34,6 +55,7 @@ const Topic = (props) => {
                         makeATopic={props.makeATopic}
                         onChange={props.handleChange}
                         onClick={props.makeATopic}
+                        convertTime={props.convertTime}
                         >
                 </ModalCard>
                 </section>
